ef923d83c6a0f622dff3d13f0ab72007978027b0
gravity182 <dm.blink19@gmail.com>
2025-12-27 01:03:11 +0300

    Add private gitlab pypi registry pwd

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  platform: 'github'
+  platform: 'github',
+
+  hostRules: [
+    {
+      matchHost: "gitlab.com",
+      hostType: "pypi",
+      username: "gitlab+deploy-token-10594845",
+      password: process.env.RENOVATE_GITLAB_PYPI_REGISTRY_PASSWORD,
+    },
+  ],
 };
